eceb9606922ec46dcfd2b6701ff4de54536e4911
Richard Kelley <richard.kelley@gmail.com>
2023-11-29 14:05:27 -0800

    Added tick() implementation to sequence_node.

--- src/dendron/controls/sequence_node.py
+++ src/dendron/controls/sequence_node.py
@@ -1,5 +1,6 @@
 from ..basic_types import NodeType, NodeStatus
 from ..tree_node import NodeConfig, TreeNode
+from ..control_node import ControlNode
 
 class SequenceNode(ControlNode):
 
@@ -13,5 +14,30 @@ class SequenceNode(ControlNode):
         ControlNode.halt(self)
 
     def tick(self):
-        pass # TODO
+        n_children = self.children_count()
+        self.set_status(NodeStatus.RUNNING)
 
+        while(self.current_child_idx < n_children):
+            current_child = self.children[self.current_child_idx]
+
+            child_status = current_child.execute_tick()
+            
+            match child_status:
+                case NodeStatus.RUNNING:
+                    return NodeStatus.RUNNING
+                case NodeStatus.FAILURE:
+                    self.reset_children()
+                    self.current_child_idx = 0
+                    return child_status
+                case NodeStatus.SUCCESS:
+                    self.current_child_idx += 1
+                case NodeStatus.SKIPPED:
+                    self.current_child_idx += 1
+                case NodeStatus.IDLE:
+                    raise RuntimeError("Child can't return IDLE")
+
+        if self.current_child_idx == n_children:
+            self.reset_children()
+            self.current_child_idx = 0
+
+        return NodeStatus.SUCCESS
