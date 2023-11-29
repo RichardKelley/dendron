4026f8d6fbeac0531a338c05a3ce9b43899e0988
Richard Kelley <richard.kelley@gmail.com>
2023-11-29 12:56:38 -0800

    Added stub for BehaviorTree class.

---
+++ src/dendron/behavior_tree.py
@@ -0,0 +1,18 @@
+from .basic_types import NodeType, NodeStatus
+from .tree_node import NodeConfig, TreeNode
+
+class BehaviorTree:
+    def __init__(self):
+        pass # TODO
+
+    def initialize(self):
+        pass # TODO
+
+    def halt_tree(self):
+        pass # TODO
+
+    def tick_once(self):
+        pass # TODO
+
+    def tick_while_running(self):
+        pass # TODO
