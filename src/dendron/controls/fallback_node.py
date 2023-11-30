f8e860876cd8342dd66b04931a01173b447023a5
Richard Kelley <richard.kelley@gmail.com>
2023-11-29 20:47:19 -0800

    Added tick() implementation for FallbackNode.

--- src/dendron/controls/fallback_node.py
+++ src/dendron/controls/fallback_node.py
@@ -14,4 +14,31 @@ class FallbackNode(ControlNode):
         ControlNode.halt(self)
 
     def tick(self):
-        pass
+        n_children = self.children_count()
+        self.set_status(NodeStatus.RUNNING)
+
+        while(self.current_child_idx < n_children):
+            current_child = self.children[self.current_child_idx]
+
+            child_status = current_child.execute_tick()
+
+            match child_status:
+                case NodeStatus.RUNNING:
+                    return NodeStatus.RUNNING
+                case NodeStatus.FAILURE:
+                    self.current_child_idx += 1
+                case NodeStatus.SUCCESS:
+                    self.reset_children()
+                    self.current_child_idx = 0
+                    return child_status
+                case NodeStatus.SKIPPED:
+                    self.current_child_idx += 1
+                case NodeStatus.IDLE:
+                    raise RuntimeError("Child can't return IDLE")
+            
+        if self.current_child_idx == n_children:
+            self.reset_children()
+            self.current_child_idx = 0
+
+        return NodeStatus.FAILURE
+
