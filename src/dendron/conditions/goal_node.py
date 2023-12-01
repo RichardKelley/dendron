e6d0ee6affd3f6b42e69ab4f079a9abeb26d51b3
Richard Kelley <richard.kelley@gmail.com>
2023-12-01 14:47:08 -0800

    Added stub for GoalNode as a new condition.

---
+++ src/dendron/conditions/goal_node.py
@@ -0,0 +1,17 @@
+from ..basic_types import NodeType, NodeStatus
+from ..tree_node import TreeNode
+from ..condition_node import ConditionNode
+
+class GoalNode(ConditionNode):
+    """
+    A GoalNode is just syntactic sugar for a condition node -
+
+    It should return SUCCESS precisely when the goal is achieved.
+
+    This may be useful for display and debugging purposes.
+    """
+    def __init__(self, name):
+        super().__init__(name)
+
+    def node_type(self) -> NodeType:
+        return NodeType.GOAL 
