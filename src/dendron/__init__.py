23c1659cefac9a12bd24ba1f3978c4c1a3465b0d
Richard Kelley <richard.kelley@gmail.com>
2023-12-07 17:21:59 -0800

    Added factory to main __init__.

--- src/dendron/__init__.py
+++ src/dendron/__init__.py
@@ -6,6 +6,7 @@ from .decorators import InverterNode
 from .action_node import ActionNode
 from .basic_types import NodeType, NodeStatus
 from .behavior_tree import BehaviorTree 
+from .behavior_tree_factory import BehaviorTreeFactory
 from .blackboard import Blackboard, BlackboardEntryMetadata
 from .condition_node import ConditionNode 
 from .control_node import ControlNode 
