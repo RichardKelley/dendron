59841cff3db0ae68e1ebacdb2aec24bb3c17beb4
Richard Kelley <richard.kelley@gmail.com>
2023-12-07 17:22:15 -0800

    Created test file for factory.

---
+++ tests/test_behavior_tree_factory.py
@@ -0,0 +1,15 @@
+from dendron import *
+
+def test_create_from_groot0():
+
+    factory = BehaviorTreeFactory()
+    factory.register_simple_action("Action1", lambda: NodeStatus.FAILURE)
+    factory.register_simple_action("Action2", lambda: NodeStatus.FAILURE)
+    factory.register_simple_condition("AtGoal", lambda: NodeStatus.FAILURE)
+    factory.register_simple_action("PortedAction1", lambda: NodeStatus.FAILURE)
+    factory.register_simple_condition("Precond1", lambda: NodeStatus.FAILURE)    
+    factory.register_simple_condition("Precond2", lambda: NodeStatus.FAILURE)
+
+    tree = factory.create_from_xml("tests/data/TestTree0.xml")
+
+    tree.pretty_print()
