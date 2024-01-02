98e53f4d031c672483be3bd32241bc9867c4ba0d
Richard Kelley <richard.kelley@gmail.com>
2024-01-02 13:42:16 -0800

    Renamed state variable in retry decorator.

--- src/dendron/decorators/retry_node.py
+++ src/dendron/decorators/retry_node.py
@@ -6,23 +6,27 @@ class RetryNode(DecoratorNode):
     def __init__(self, name: str, child: TreeNode, n_times: int):
         super().__init__(self, child)
         self.n_times = n_times
-        self.repeat_ct = 0
+        self.retry_ct = 0
+
+    def reset(self):
+        self.retry_ct = 0
+        self.child_node.reset()
 
     def tick(self):
-        should_repeat = True
+        should_retry = True
         
         while should_repeat:
             child_status = self.child_node.execute_tick()
             match child_status:
                 case NodeStatus.SUCCESS:
-                    self.repeat_ct = 0
+                    self.retry_ct = 0
                     self.reset_child()
                     return NodeStatus.SUCCESS
                 case NodeStatus.FAILURE:
-                    self.repeat_ct += 1
-                    should_repeat = self.repeat_ct < self.n_times
+                    self.retry_ct += 1
+                    should_retry = self.retry_ct < self.n_times
                 case NodeStatus.RUNNING:
                     return NodeStatus.RUNNING
 
-        self.repeat_ct = 0
+        self.retry_ct = 0
         return NodeStatus.FAILURE
