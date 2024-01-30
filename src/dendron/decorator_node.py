802339c791152d9562deeb25ac9ad2488f2ce3e3
Richard Kelley <richard.kelley@gmail.com>
2024-01-30 11:24:49 -0800

    Added doc comments to Decorator class.

--- src/dendron/decorator_node.py
+++ src/dendron/decorator_node.py
@@ -9,26 +9,62 @@ import logging
 BehaviorTree = typing.NewType("BehaviorTree", None)
 
 class DecoratorNode(TreeNode):
+    """
+    A decorator is a "wrapper" around a single node. The purpose of 
+    the decorator is to modify or support the action of its child in
+    some way.
+
+    Args:
+        name (`str`):
+            The given name of this node.
+        child (`dendron.tree_node.TreeNode`):
+            An optional child node. If not specified, the child must be 
+            set before this node's `tick()` function is first called.
+    """
 
     def __init__(self, name, child : TreeNode = None) -> None:
         super().__init__(name)
         self.child_node : TreeNode = child
 
     def set_logger(self, new_logger) -> None:
+        """
+        Set the logger for this node, and then forward the logger to the 
+        child node.
+        """
         self.logger = new_logger
         self.child_node.set_logger(new_logger)
 
     def set_log_level(self, new_level) -> None:
+        """
+        Set the log level for this node, and then forward that level to 
+        the child node.
+        """
         self.log_level = new_level
         self.child_node.set_log_level(new_level)
 
     def node_type(self) -> NodeType:
+        """
+        Return this node's type.
+        """
         return NodeType.DECORATOR 
 
-    def set_child(self, child) -> None:
+    def set_child(self, child : TreeNode) -> None:
+        """
+        Set the child of this node to a new `TreeNode`.
+
+        Args:
+            child (`dendron.tree_node.TreeNode`):
+                The new child of this decorator.
+        """
         self.child_node = child
 
     def get_child(self) -> TreeNode:
+        """
+        Get the child of this decorator.
+
+        Returns:
+            `TreeNode`: The child of this node.
+        """
         return self.child_node
 
     def get_node_by_name(self, name : str) -> Optional[TreeNode]:
@@ -49,13 +85,29 @@ class DecoratorNode(TreeNode):
             return self.child_node.get_node_by_name(name)
 
     def halt_child(self) -> None:
+        """
+        Instruct the child node to halt.
+        """
         self.child_node.halt_node()
 
     def set_tree(self, tree : BehaviorTree) -> None:
+        """
+        Set the tree of this node, and then forward the tree to the child
+        to have it set its tree.
+
+        Args:
+            tree (`dendron.behavior_tree.BehaviorTree`):
+                The tree that contains this node.
+        """
         self.tree = tree
         self.child_node.set_tree(tree)
 
     def reset(self) -> None:
+        """
+        Set the status of this node to IDLE and instruct the child node to
+        reset.
+        """
+        self.node_status = NodeStatus.IDLE
         self.child_node.reset()
 
     def pretty_repr(self, depth = 0) -> str:
