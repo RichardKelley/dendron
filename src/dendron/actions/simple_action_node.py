fa720524918183ce2a36afff180fc088f232c471
Richard Kelley <richard.kelley@gmail.com>
2024-01-31 08:50:16 -0800

    Added doc comments to simple action node.

--- src/dendron/actions/simple_action_node.py
+++ src/dendron/actions/simple_action_node.py
@@ -1,10 +1,27 @@
 from ..action_node import ActionNode
 from ..basic_types import NodeStatus
 
+from typing import Callable
+
 class SimpleActionNode(ActionNode):
-    def __init__(self, name, callback) -> None:
+    """
+    A simple action node is initialized with a callback that is 
+    called every time this node `tick()`s. The callback should
+    be a function that that returns a `NodeStatus`.
+
+    Args:
+        name (`str`):
+            The given name of this node.
+        callback (`Callable`):
+            The callback to be executed upon every `tick()`.
+    """
+    def __init__(self, name : str, callback : Callable) -> None:
         super().__init__(name)
         self.callback = callback
 
     def tick(self) -> NodeStatus:
+        """
+        Call the callback function and return its status as the
+        node status.
+        """
         return self.callback() 
