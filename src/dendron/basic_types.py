5e7df291652c9aebbf33c0c1d873f3260be0c0b3
Richard Kelley <richard.kelley@gmail.com>
2024-02-19 22:55:50 -0800

    Added comments for the types in basic_types.

--- src/dendron/basic_types.py
+++ src/dendron/basic_types.py
@@ -1,6 +1,10 @@
 from enum import Enum
 
 class NodeType(Enum):
+    """
+    Enum containing the types of nodes allowed in the behavior tree
+    framework. Some of these are experimental.
+    """
     UNDEFINED = 0
     ACTION = 1
     CONDITION = 2
@@ -12,6 +16,10 @@ class NodeType(Enum):
     SUBTREE = 8
 
 class NodeStatus(Enum):
+    """
+    Enum containing the allowable return values from node `tick` 
+    functions.
+    """
     IDLE = 0
     RUNNING = 1
     SUCCESS = 2
@@ -19,6 +27,10 @@ class NodeStatus(Enum):
     SKIPPED = 4
 
 class Quantization(Enum):
+    """
+    Enum representing currently allowable quantization levels for
+    neural models. `TwoBit` is currently aspirational.
+    """
     NoQuantization = 0, 
     TwoBit = 2,
     FourBit = 4,
