637022eb7a2e54b40f12915beec256461f153658
Richard Kelley <richard.kelley@gmail.com>
2024-02-22 17:02:33 -0800

    bump patch number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "dendron",
-    version = "0.0.1",
+    version = "0.1.1",
     author = "Richard Kelley",
     package_dir = {"" : "src"},
     packages = setuptools.find_packages(where="src"),
