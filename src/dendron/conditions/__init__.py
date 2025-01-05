c31e1cf97a1e8800ca7e26eafed91c1e4ccbcfe6
Richard Kelley <richard.kelley@gmail.com>
2025-01-04 19:53:00 -0800

    __init__ files updated.

--- src/dendron/conditions/__init__.py
+++ src/dendron/conditions/__init__.py
@@ -2,4 +2,4 @@ from .goal_node import GoalNode
 from .conjunction_node import ConjunctionNode
 from .disjunction_node import DisjunctionNode 
 from .simple_condition import SimpleCondition
-from .completion_condition import CompletionCondition
+from .lm_completion_condition import LMCompletionCondition
--- src/dendron/configs/__init__.py
+++ src/dendron/configs/__init__.py
@@ -1,2 +1,3 @@
-from .hflm_action_config import HFLMActionConfig
-from .hflm_completion_config import HFLMCompletionConfig
+from .lm_action_config import LMActionConfig
+from .lm_completion_config import LMCompletionConfig
+from .hflm_config import HFLMConfig
