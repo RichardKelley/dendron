dee995001afc33d94cf5809aac10fe84cc70c618
Richard Kelley <richard.kelley@gmail.com>
2025-01-13 19:16:10 -0600

    Added a util file to get a default device.

---
+++ src/dendron/util.py
@@ -0,0 +1,9 @@
+import torch
+
+def default_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
